8aafa9e66ed30e98d140d41c367c23b5af6f519b
pripac <pristudy96@gmail.com>
2022-03-10 12:36:17 -0300

    js not working (?)
    improve visual by adding an "chicago" image

--- aluraflix_IDa4.js
+++ aluraflix_IDa4.js
@@ -1,11 +1,15 @@
+alert("Hello word")
 
 function Show(){
     var music = document.getElementById("video").value
 
-    alert(music)
 
+    alert("Test")}
 
 }
 
+
+
+// array = {"All that Jazz"; "Funny Honey"; "When You're Good To Mama"; "Cell Block Tango";"All I Care About"; "We Both Reached For The Gun"; "Roxie";"I Can't Do It Alone";"Mister Cellophane"; "Razzle Dazzle"; "Nowadays"}
 
 //if (music == "Cellblock Tango"){music.innerHTML = <iframe width="560" height="315" src="https://www.youtube.com/embed/Amr15YjPdgg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>} else {alert("Test")}
