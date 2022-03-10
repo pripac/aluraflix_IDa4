196dcaa198a7cdf194edc11997aedcfc1f99ac30
pripac <pristudy96@gmail.com>
2022-03-10 10:56:53 -0300

    basic structure

---
+++ aluraflix_IDa4.js
@@ -0,0 +1,11 @@
+
+function Show(){
+    var music = document.getElementById("video").value
+
+    alert(music)
+
+
+}
+
+
+//if (music == "Cellblock Tango"){music.innerHTML = <iframe width="560" height="315" src="https://www.youtube.com/embed/Amr15YjPdgg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>} else {alert("Test")}
